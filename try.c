e98c1a924926aa86a6f61212440eed09dbaddf8b
aimanyounises1 <aymanyounis@icloud.com>
2020-05-26 22:05:24 +0300

    fixed bugs

--- try.c
+++ try.c
@@ -15,10 +15,11 @@ int fd[2];
 int val = 0;
 sig_atomic_t sig_num;
  int counter;
- void child(void)
+ void parent(pid_t pid);
+ void child()
 {
     sleep(1);
-    write(fd[0], &val, sizeof(val));  
+    write(fd[1], &val, sizeof(val));  
     kill(getppid(), SIGUSR1); 
     while (1)
     {
@@ -26,11 +27,10 @@ sig_atomic_t sig_num;
 	if(val < 5){
 		sleep(1); 
 		val++;
-		write(fd[0], &val, sizeof(val));
+		write(fd[1], &val, sizeof(val));
 		kill(getppid(), SIGUSR1); 
 	}
-	else{
-	  
+	else{ 
 	   printf("Parent is going to be terminated\n");
 	   break;
 	}
@@ -46,38 +46,37 @@ sig_atomic_t sig_num;
 {
 	
     while(1){
-	
-    	read(fd[1], &val, sizeof(val));
-	
+    	read(fd[0], &val, sizeof(val));
     	printf(" 	%d\n",val);
-	
     	if(val < 5){
 		sleep(1);
 		val++;
 		write(fd[1], &val, sizeof(val));
-    		kill(pid, SIGUSR1);
+    	kill(pid, SIGUSR1);
     	}
     	else{
     		printf("Child is going to be terminated\n");
-		write(fd[1], &val, sizeof(val));
-		kill(pid, SIGUSR1);
+		    write(fd[1], &val, sizeof(val));
+			kill(pid, SIGUSR1);
+		    
 		break;
     	}
     }
-    close(fd[1]);
+   close(fd[1]);
 }
 
 int main()
 {
-	pipe(fd);
-    struct sigaction SA;
-    sigemptyset(&SA.sa_mask);
+	pipe(fd);// pipe fd[0] to read and fd[1] to write
+     struct sigaction SA;
+    sigemptyset(&SA.sa_mask);// setting the signal mask to empty 
     SA.sa_flags = 0;
-    SA.sa_handler = fun;
+    SA.sa_handler = fun;// pointer to the catch function 
     if (sigaction(SIGUSR1, &SA, NULL) == -1){
         printf("Failed");
 	exit(2);
     }
+	// startring chiled process
     pid_t pid = fork();
     if (pid < 0){
         printf("Fail");
